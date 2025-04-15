653cd7a075c43929f3f9fde21b9ac34d7fa8f02b
FLaNEUR-Q <88171815+FLaNEUR-Q@users.noreply.github.com>
2025-04-16 01:24:49 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -23,8 +23,8 @@ export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 7] }}>
-        <ambientLight intensity={0.6} />
-        <directionalLight position={[5, 0, 5]} intensity={2} />
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[3, 2, 5]} intensity={2} />
         <Stars radius={100} depth={50} count={5000} factor={4} fade speed={1} />
         <Earth />
         <OrbitControls />
