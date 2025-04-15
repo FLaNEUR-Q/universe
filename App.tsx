b19507a7c7befccadb21873aee3a334bdd721050
FLaNEUR-Q <88171815+FLaNEUR-Q@users.noreply.github.com>
2025-04-16 01:29:31 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,18 +4,27 @@ import { OrbitControls, Stars } from '@react-three/drei'
 import * as THREE from 'three'
 
 function Earth() {
-  const ref = useRef()
+  const planetRef = useRef()
+  const atmosphereRef = useRef()
+
   const texture = new THREE.TextureLoader().load("https://cdn.jsdelivr.net/gh/rajatdiptabiswas/planet-textures@main/2k_earth_daymap.jpg")
 
   useFrame(() => {
-    if (ref.current) ref.current.rotation.y += 0.0015
+    if (planetRef.current) planetRef.current.rotation.y += 0.0015
+    if (atmosphereRef.current) atmosphereRef.current.rotation.y += 0.0015
   })
 
   return (
-    <mesh ref={ref}>
-      <sphereGeometry args={[2, 64, 64]} />
-      <meshStandardMaterial map={texture} />
-    </mesh>
+    <>
+      <mesh ref={planetRef}>
+        <sphereGeometry args={[2, 64, 64]} />
+        <meshStandardMaterial map={texture} />
+      </mesh>
+      <mesh ref={atmosphereRef}>
+        <sphereGeometry args={[2.15, 64, 64]} />
+        <meshBasicMaterial color="#aadfff" transparent opacity={0.1} side={THREE.BackSide} />
+      </mesh>
+    </>
   )
 }
 
@@ -23,8 +32,8 @@ export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 7] }}>
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[3, 2, 5]} intensity={2} />
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[5, 2, 5]} intensity={2} />
         <Stars radius={100} depth={50} count={5000} factor={4} fade speed={1} />
         <Earth />
         <OrbitControls />
